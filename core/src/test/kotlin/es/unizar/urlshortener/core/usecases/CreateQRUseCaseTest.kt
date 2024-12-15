da372bd0751662be2c5ad06c3a2958cb63bf59ad
Lilai <840091@unizar.es>
2024-12-15 19:47:44 +0100

    Added QR tests

--- core/src/test/kotlin/es/unizar/urlshortener/core/usecases/CreateQRUseCaseTest.kt
+++ core/src/test/kotlin/es/unizar/urlshortener/core/usecases/CreateQRUseCaseTest.kt
@@ -6,10 +6,10 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNull
-import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito.*
-import org.mockito.kotlin.whenever
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import javax.imageio.ImageIO
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
@@ -33,5 +33,39 @@ class CreateQRUseCaseTest {
 
         assertNotNull(qrCodeBytes)
         assertTrue(qrCodeBytes.isNotEmpty())
+    }
+
+    @Test
+    fun `should generate a PNG format QR code`() {
+        val url = "https://example.com"
+        val size = 300
+
+        val bitMatrix = BitMatrix(size, size)
+        `when`(qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, size, size)).thenReturn(bitMatrix)
+
+        val qrCodeBytes = createQRUseCase.create(url, size)
+
+        val inputStream = ByteArrayInputStream(qrCodeBytes)
+        val bufferedImage: BufferedImage = ImageIO.read(inputStream)
+
+        assertNotNull(bufferedImage)
+        val formatName = ImageIO.getImageReadersBySuffix("png").next().formatName.toLowerCase()
+
+        assertEquals("png", formatName)
+    }
+
+    @Test
+    fun `should return QR code with correct size`() {
+        val url = "https://example.com"
+        val size = 300
+
+        val bitMatrix = BitMatrix(size, size)
+        `when`(qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, size, size)).thenReturn(bitMatrix)
+
+        val qrCodeBytes = createQRUseCase.create(url, size)
+
+        val image = ImageIO.read(qrCodeBytes.inputStream())
+        assertEquals(size, image.width)
+        assertEquals(size, image.height)
     }
 }
