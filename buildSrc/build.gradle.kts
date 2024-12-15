f043a24f62152d44eb061fce8a1161c5767a081a
sergioes55 <96990703+sergioes55@users.noreply.github.com>
2024-12-15 13:28:21 +0100

    check sonarLint

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     // Applies the Kotlin DSL plugin to the project.
     `kotlin-dsl`
+    id("org.sonarqube") version "4.4.1.3373"
 }
+subprojects {
+    apply(plugin = "org.sonarqube") // Aplica el plugin a los subproyectos
+}
+
 
 repositories {
     // Adds the Maven Central repository to the list of repositories.
