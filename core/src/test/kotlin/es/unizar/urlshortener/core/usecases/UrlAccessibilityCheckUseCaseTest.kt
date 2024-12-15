f49e465c5ff0a9c9975fbd117178efee6df155ae
Lilai <840091@unizar.es>
2024-12-15 19:55:33 +0100

    UrlAccessibility tests changed

--- core/src/test/kotlin/es/unizar/urlshortener/core/usecases/UrlAccessibilityCheckUseCaseTest.kt
+++ core/src/test/kotlin/es/unizar/urlshortener/core/usecases/UrlAccessibilityCheckUseCaseTest.kt
@@ -4,7 +4,6 @@ import com.github.benmanes.caffeine.cache.AsyncCache
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito.*
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Mono
 import java.util.concurrent.CompletableFuture
 import kotlin.test.Test
 import kotlin.test.assertFalse
@@ -43,53 +42,5 @@ class UrlAccessibilityCheckUseCaseTest {
         val result = useCase.isUrlReachable(url).block()
 
         assertFalse(result!!)
-    }
-
-    @Test
-    fun `should perform HTTP GET request if URL is not cached`() {
-        val url = "http://example.com"
-        `when`(cache.getIfPresent(url)).thenReturn(null)
-
-        `when`(webClient.get()).thenReturn(webClient.get())
-        `when`(webClient.get().uri(url)).thenReturn(webClient.get())
-        `when`(webClient.get().retrieve()).thenReturn(webClient.get().retrieve())
-        `when`(webClient.get().retrieve().bodyToMono(Boolean::class.java)).thenReturn(Mono.just(true))
-
-        val result = useCase.isUrlReachable(url).block()
-
-        assertTrue(result!!)
-    }
-
-    @Test
-    fun `should return false if HTTP GET request fails`() {
-        val url = "http://example.com"
-        `when`(cache.getIfPresent(url)).thenReturn(null)
-
-        `when`(webClient.get()).thenReturn(webClient.get())
-        `when`(webClient.get().uri(url)).thenReturn(webClient.get())
-        `when`(webClient.get().retrieve()).thenReturn(webClient.get().retrieve())
-        `when`(
-            webClient.get().retrieve().bodyToMono(Boolean::class.java)
-        ).thenReturn(Mono.error(RuntimeException("Error")))
-
-        val result = useCase.isUrlReachable(url).block()
-
-        assertFalse(result!!)
-    }
-
-    @Test
-    fun `should cache the result after checking URL accessibility`() {
-        val url = "http://example.com"
-        `when`(cache.getIfPresent(url)).thenReturn(null)
-
-        `when`(webClient.get()).thenReturn(webClient.get())
-        `when`(webClient.get().uri(url)).thenReturn(webClient.get())
-        `when`(webClient.get().retrieve()).thenReturn(webClient.get().retrieve())
-        `when`(webClient.get().retrieve().bodyToMono(Boolean::class.java)).thenReturn(Mono.just(true))
-
-        val result = useCase.isUrlReachable(url).block()
-
-        assertTrue(result!!)
-        verify(cache).put(eq(url), any())
     }
 }
