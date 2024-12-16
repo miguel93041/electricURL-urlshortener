63bc437e8d519fa8b7aee8dba999a7bd91f1ae74
Lilai <840091@unizar.es>
2024-12-16 02:27:22 +0100

    Refactor duplicated blocks in BrowserPlatformIdentificationUseCaseTest

--- core/src/test/kotlin/es/unizar/urlshortener/core/usecases/BrowserPlatformIdentificationUseCaseTest.kt
+++ core/src/test/kotlin/es/unizar/urlshortener/core/usecases/BrowserPlatformIdentificationUseCaseTest.kt
@@ -1,7 +1,9 @@
 package es.unizar.urlshortener.core.usecases
 
 import es.unizar.urlshortener.core.BrowserPlatform
-import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.whenever
 import ua_parser.Client
@@ -56,86 +58,65 @@ class BrowserPlatformIdentificationUseCaseTest {
         assertEquals("Unknown", result.platform)
     }
 
-    @Test
-    fun `should identify Safari on macOS`() {
-        val userAgentString =
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)" +
-                    "Version/14.0 Safari/605.1.15"
-
-        val userAgent = UserAgent("Safari", "14", "0", null)
-        val os = OS("macOS", "10.15.7", "", "", "")
-        val client = Client(userAgent, os, null)
+    @ParameterizedTest
+    @MethodSource("provideUserAgentData")
+    fun `should identify commons browser and platform correctly`(
+        userAgentString: String,
+        expectedUserAgent: UserAgent,
+        expectedOs: OS,
+        expectedResult: BrowserPlatform
+    ) {
+        val client = Client(expectedUserAgent, expectedOs, null)
 
         whenever(parser.parse(userAgentString)).thenReturn(client)
 
         val result = browserPlatformIdentificationUseCase.parse(userAgentString)
 
-        assertEquals(BrowserPlatform("Safari", "macOS"), result)
+        assertEquals(expectedResult, result)
     }
 
-    @Test
-    fun `should identify Firefox on Linux`() {
-        val userAgentString = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
-
-        val userAgent = UserAgent("Firefox", "91", "0", null)
-        val os = OS("Linux", null, "", "", "")
-        val client = Client(userAgent, os, null)
-
-        whenever(parser.parse(userAgentString)).thenReturn(client)
-
-        val result = browserPlatformIdentificationUseCase.parse(userAgentString)
-
-        assertEquals(BrowserPlatform("Firefox", "Linux"), result)
-    }
-
-    @Test
-    fun `should identify Edge on Windows`() {
-        val userAgentString =
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
-                    "Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"
-
-        val userAgent = UserAgent("Edge", "91", "0", "864")
-        val os = OS("Windows", "10", "", "", "")
-        val client = Client(userAgent, os, null)
-
-        whenever(parser.parse(userAgentString)).thenReturn(client)
-
-        val result = browserPlatformIdentificationUseCase.parse(userAgentString)
-
-        assertEquals(BrowserPlatform("Edge", "Windows"), result)
-    }
-
-    @Test
-    fun `should identify Chrome on Android`() {
-        val userAgentString =
-            "Mozilla/5.0 (Linux; Android 11; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) " +
-                    "Chrome/91.0.4472.124 Mobile Safari/537.36"
-
-        val userAgent = UserAgent("Chrome", "91", "0", "4472")
-        val os = OS("Android", "11", "", "", "")
-        val client = Client(userAgent, os, null)
-
-        whenever(parser.parse(userAgentString)).thenReturn(client)
-
-        val result = browserPlatformIdentificationUseCase.parse(userAgentString)
-
-        assertEquals(BrowserPlatform("Chrome", "Android"), result)
-    }
-
-    @Test
-    fun `should identify Safari on iOS`() {
-        val userAgentString =
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) " +
-                    "Version/14.1 Mobile/15E148 Safari/604.1"
-
-        val userAgent = UserAgent("Safari", "14", "1", null)
-        val os = OS("iOS", "14.6", "", "", "")
-        val client = Client(userAgent, os, null)
-
-        whenever(parser.parse(userAgentString)).thenReturn(client)
-
-        val result = browserPlatformIdentificationUseCase.parse(userAgentString)
-
-        assertEquals(BrowserPlatform("Safari", "iOS"), result)
+    companion object {
+        @JvmStatic
+        fun provideUserAgentData() = listOf(
+            Arguments.of(
+                // Safari on macOS
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)" +
+                    "Version/14.0 Safari/605.1.15",
+                UserAgent("Safari", "14", "0", null),
+                OS("macOS", "10.15.7", "", "", ""),
+                BrowserPlatform("Safari", "macOS")
+            ),
+            Arguments.of(
+                // Firefox on Linux
+                "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0",
+                UserAgent("Firefox", "91", "0", null),
+                OS("Linux", null, "", "", ""),
+                BrowserPlatform("Firefox", "Linux")
+            ),
+            Arguments.of(
+                // Edge on Windows
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+                    "Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",
+                UserAgent("Edge", "91", "0", "864"),
+                OS("Windows", "10", "", "", ""),
+                BrowserPlatform("Edge", "Windows")
+            ),
+            Arguments.of(
+                // Chrome on Android
+                "Mozilla/5.0 (Linux; Android 11; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) " +
+                    "Chrome/91.0.4472.124 Mobile Safari/537.36",
+                UserAgent("Chrome", "91", "0", "4472"),
+                OS("Android", "11", "", "", ""),
+                BrowserPlatform("Chrome", "Android")
+            ),
+            Arguments.of(
+                // Safari on iOS
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) " +
+                    "Version/14.1 Mobile/15E148 Safari/604.1",
+                UserAgent("Safari", "14", "1", null),
+                OS("iOS", "14.6", "", "", ""),
+                BrowserPlatform("Safari", "iOS")
+            )
+        )
     }
 }
